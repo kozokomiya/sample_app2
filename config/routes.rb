e6342b742a7ee995c631d77ccb8e1d18810b64c8
Kozo Komiya <kozo.komiya@gmail.com>
2018-04-25 09:13:37 +0000

    Finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root 'static_pages#home'                      # => root_path
+  get  '/help',    to: 'static_pages#help'      # => help_path
+  get  '/about',   to: 'static_pages#about'     # => about_path
+  get  '/contact', to: 'static_pages#contact'   # => contact_path
+  get  '/signup',  to: 'users#new'              # => signup_path
 end
