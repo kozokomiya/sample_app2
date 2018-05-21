61b9c2e5be3097485309d2f819af5b9a87160348
Kozo Komiya <kozo.komiya@gmail.com>
2018-05-21 03:27:08 +0000

    finish 11.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :account_activations, only: [:edit]
+    # => GET /account_activations/:id/edit"
+    # params[:id] <== 有効化トークン
+    # Controller: params[:id]
 end
